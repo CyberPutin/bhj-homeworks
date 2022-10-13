a6852ea714bfadfe24d7e60a2f8ebfd0d683823d
Regina <rigifox@mail.ru>
2022-10-13 16:31:12 +0300

    changed task.js of the first commit interests

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -3,30 +3,26 @@ const checkbox = Array.from(document.getElementsByClassName('interest__check'));
 function onCheckedParents(e) {
     let check = e.target;
 
-    if (!checkbox.indexOf(check)) {
-        return;
-    } else {
-        while (check) {
-            const parent = check.closest('ul').parentElement.querySelector('input');
-            const sibling = Array.from((parent.closest('li').querySelector('ul')).querySelectorAll('input'));
-            const checkStatus = sibling.map(elem => elem.checked === true);
-            const every = checkStatus.every(elem => elem === true);
-            const some = checkStatus.some(elem => elem === true);
-            parent.checked = every;
-
-            if (!every && every !== some) {
-                parent.indeterminate = true;
+    if (!checkbox.indexOf(check)) return;
+    while (check) {
+        const parent = check.closest('ul').parentElement.querySelector('input');
+        const sibling = Array.from((parent.closest('li').querySelector('ul')).querySelectorAll('input'));
+        const checkStatus = sibling.map(elem => elem.checked === true);
+        const every = checkStatus.every(elem => elem === true);
+        const some = checkStatus.some(elem => elem === true);
+        parent.checked = every;
+        if (!every && every !== some) {
+            parent.indeterminate = true;
             } else {
                 parent.indeterminate = false;
             };
 
-            if (check != parent) {
-                check = parent;
-            } else {
-                check = 0;
-            };
+        if (check != parent) {
+            check = parent;
+        } else {
+            check = 0;
         };
-    }
+    };
 }
 
 function onCheckedChildren(e) {
