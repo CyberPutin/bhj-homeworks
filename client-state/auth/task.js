3d817c3971e7b2a4e715f85adfdc26f5dc3f84d0
Regina <rigifox@mail.ru>
2022-10-17 17:22:08 +0300

    Changed task.js of the first commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,35 +1,62 @@
-const signin = document.getElementById('signin');
-const form = document.querySelector('form');
-const btn = document.getElementById('signin__btn');
-const welcome = document.getElementById('welcome');
+let signin = document.getElementById("signin");
+signin.classList.add("signin_active");
 
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  let xhr = new XMLHttpRequest();
-  let formData = new FormData(form);
+let signinForm = signin.querySelector("#signin__form");
 
-  xhr.addEventListener('load', () => {
-    if (xhr.readyState === 4) {
-      let response = JSON.parse(xhr.response);
-      if (response.success === false) {
-        alert('Неверный логин/пароль');
-      } else {
-        localStorage.setItem('user_id', response['user_id']);
-        signin.classList.remove('signin_active');
-        welcome.classList.add('welcome_active');
-        welcome.querySelector('span').textContent = response['user_id'];
-      }
-    }
-  });
+function clearForm() {
+	signinForm.querySelector(`[name="login"]`).value = "";
+	signinForm.querySelector(`[name="password"]`).value = "";
+}
 
-  xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-  xhr.send(formData);
-});
+function signIN(e) {
+	e.preventDefault();
+	let xhr = new XMLHttpRequest();
+	xhr.addEventListener("loadend", function () {
+		let response = this.response;
+		if (response.success) {
+			localStorage.setItem("user_id", response.user_id);
+		} else {
+			alert("Неверный логин/пароль");
+		}
+		showWelcome();
+	});
+	xhr.responseType = "json";
+	xhr.open("POST", this.closest("form").action);
+	xhr.send(new FormData(this.closest("form")));
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  if (localStorage['user_id']) {
-    signin.classList.remove('signin_active');
-    welcome.classList.add('welcome_active');
-    welcome.querySelector('span').textContent = localStorage['user_id'];
-  }
-});
+function signOUT(e) {
+	e.preventDefault();
+	localStorage.removeItem("user_id");
+	showWelcome();
+};
+
+function showWelcome() {
+	let userID = localStorage.getItem("user_id");
+	let welcomeWindow = document.getElementById("welcome");
+	if (userID) {
+		welcomeWindow.querySelector("#user_id").textContent = userID;
+		welcomeWindow.classList.add("welcome_active");
+		signinForm.querySelector(`[name="login"]`).style.display = "none";
+		signinForm.querySelector(`[name="password"]`).style.display = "none";
+		signinForm.querySelector("#signin__btn").style.display = "none";
+		signinForm.querySelector("#signout__btn").style.display = "block";
+	} else {
+		signinForm.querySelector(`[name="login"]`).style.display = "block";
+		signinForm.querySelector(`[name="password"]`).style.display = "block";
+		signinForm.querySelector("#signout__btn").style.display = "none";
+		signinForm.querySelector("#signin__btn").style.display = "block";
+		welcomeWindow.querySelector("#user_id").textContent = "";
+		welcomeWindow.classList.remove("welcome_active");
+	}
+	clearForm();
+};
+
+signinForm
+	.querySelector("#signin__btn")
+	.addEventListener("click", signIN, false);
+signinForm
+	.querySelector("#signout__btn")
+	.addEventListener("click", signOUT, false);
+
+showWelcome();
